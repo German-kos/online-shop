7d8453632188573171daa31a07d82bf90860c3d8
German <KostiakovG@Gmail.com>
2022-03-18 17:17:12 +0200

    login prototype done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import { Test } from "./components/component-test";
 import { Login } from "./components/login-prot";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+// import { Test } from "./components/component-test";
 //
 function App() {
+  const [user, setUser] = useState(undefined);
+  //
+  const setAuthenticatedUser = (e) => {
+    e.preventDefault();
+    axios.get("./files/users.json").then((res) => {
+      setUser(
+        res.data.find(
+          (obj) =>
+            obj.username.toLowerCase === e.target[0].value.toLowerCase &&
+            obj.password === e.target[1].value
+        )
+      );
+    });
+  };
+  //testing to see if user pulls obj from json
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+  //conditional render for logged in users
+  if (user !== undefined) {
+    return <h1>Welcome, {user.username}</h1>;
+  }
   return (
     <div>
-      <header>
-        <Login/>
-      </header>
+      <Login setPass={setAuthenticatedUser} />
     </div>
   );
 }
 
 export default App;
-//
